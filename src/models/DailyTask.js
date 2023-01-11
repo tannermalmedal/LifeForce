fcadb213a871582f272a544a1aeaa3fce9578bba
Tanner Malmedal <tannermalmedal@Tanners-MacBook-Pro.local>
2023-01-10 21:06:37 -0800

    pushing the rest of the files that werent in previous commit

---
+++ src/models/DailyTask.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const DailyTaskSchema = new Schema({
+    you: {type: Boolean, default:false},
+    others: {type:Boolean, default: false},
+    yourBody: {type:Boolean, default: false},
+    youMessage: {type:String, default: "Nothing Added"},
+    othersMessage: {type: String, default: "Nothing Added"},
+    yourBodyMessage: String,
+    complete: {type: Boolean, default: false},
+    time: {type: Date, default: Date.now()}
+})
+
+const DailyTask = mongoose.model("DailyTask", DailyTaskSchema);
+
+module.exports = DailyTask;
---
+++ src/server.js
@@ -0,0 +1,65 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+mongoose.connect("mongodb://127.0.0.1:27017/lifeforce",{
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+})
+.then(() => console.log("Connected to DB"))
+.catch(console.error);
+
+const DailyTask = require('./models/DailyTask')
+
+app.get('/dailytasks', (req, res) => {
+    DailyTask.find().then((data) => {
+        res.json(data);
+    });
+})
+
+app.post('/dailytasks/new', (req,res) => {
+    DailyTask.create({
+        youMessage: req.body.youMessage,
+        othersMessage: req.body.othersMessage,
+        yourBodyMessage: req.body.yourBodyMessage,
+    }, (err, task) => {
+        if(err){
+            console.log(err);
+        }else return res.json(task);
+    })
+})
+
+app.delete('/dailytasks/delete/:id', (req, res) => {
+    console.log(req.params.id);
+    DailyTask.findOneAndDelete({_id: req.params.id}, (err, task) => {
+        if(err){
+            console.log(`No Daily Task Found, couldn't delete. ${err}`);
+        }else {
+            console.log('task deleted');
+            return res.status(200).json(task);
+        }
+    })
+})
+
+app.put('/dailytasks/complete/:id', (req, res) => {
+    DailyTask.findOne({_id:req.params.id}, (err, task) => {
+        if(err){
+            console.log(err);
+        } else{
+            task.complete === true ? task.complete = false : task.complete = true;
+            task.save();
+            res.status(200).json(task);
+        }
+    })
+})
+
+
+
+
+
+app.listen(3000, () => console.log("Server started on port 3000"));
