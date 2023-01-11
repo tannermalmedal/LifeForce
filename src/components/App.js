58d2d5e9556fd7c0a481ad4b675082f4e88f2e42
Tanner Malmedal <tannermalmedal@Tanners-MacBook-Pro.local>
2023-01-10 19:06:23 -0800

    finished up server, and tested requests to make sure they work properly (get, post, put, delete)

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,11 @@
 import React, { Component } from 'react';
 
-class App extends Component{
-
-    render(){
-        return(
-            <div>
-                <h1>Hello world we made it!</h1>
-            </div>
-        )
-    }
-}
+function App(){
+    return(
+        <div>
+            <h1>Hello world we made it!</h1>
+        </div>
+    )
+ }
 
 export default App;
